c727c991de76e9cf1c6ffa069436e22d819c79c8
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-09-18 10:58:27 +0500

    new

--- pages/auth/registration.vue
+++ pages/auth/registration.vue
@@ -1,10 +1,33 @@
 <template >
-    <div>
-
+    <div class="w-screen h-screen flex justify-center items-center bg-[#EEEDF2]">
+        <div class="w-[95%] flex-col h-[90%] flex  justify-center items-start  gap-10">
+            <h1>DLA</h1>
+            <div class="w-full h-[800px] flex justify-center items-center bg-[#027DFC] boxBorder">
+                <!-- <div class="box flex flex-col justify-between items-center">
+                    <h2>Welcome to DLA</h2>
+                    <p>Let’s set up your account together</p>
+                </div> -->
+            </div>
+        </div>
     </div>
 </template>
 <script>
 
 </script>
 <style scoped>
+h1 {
+    color: #000;
+    font-family: 'Figtree', ;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+}
+.boxBorder {
+    border-radius: 40px;
+    position: relative;
+}
+
+
+
 </style>
