e41d0cefc93124400116b83c5bc8fd92fa2d5dcf
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-10-12 12:14:10 +0500

    bacup

--- components/carusel-component.vue
+++ components/carusel-component.vue
@@ -2,9 +2,8 @@
     <div id="bannerSlider">
         <div class="bannerSliderContainer" :style="{ animationPlayState: control }">
             <div class="bannerSliderItem">
-                <div class="bannerSliderItem-image bannerSliderItem-top">
-                    <!-- style="background-image: url('https://sinfxona.uz/img/slider/1.png')" -->
-                    <nuxtImg src="https://sinfxona.uz/img/slider/1.png" format="webp" />
+                <div class="bannerSliderItem-image bannerSliderItem-top"
+                    style="background-image: url('https://sinfxona.uz/img/slider/1.png')">
                 </div>
                 <div class="bannerSliderItem-image bannerSliderItem-bottom"
                     style="background-image: url('https://sinfxona.uz/img/slider/2.png')"></div>
